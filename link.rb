277ad45a07102c3ac40e1d354cd964bdca5eaf84
Tats <vinnie.vinnie44@gmail.com>
2017-12-02 10:19:27 +0100

    initial commit

---
+++ link.rb
@@ -0,0 +1,17 @@
+class Link < Post #child of Post
+	
+	def initialize
+		super #takes the same method from Post
+
+		@url = ''
+	end
+
+	def read_from_console
+
+	end
+
+	def to_strings
+
+	end
+	
+end
---
+++ memo.rb
@@ -0,0 +1,12 @@
+class Memo < Post
+	#don't need to initialize, because no other methods than in Post are needed
+
+	def read_from_console
+
+	end
+
+	def to_strings
+
+	end
+
+end
